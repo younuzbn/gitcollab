4cbf5ca4331cb9230d5fc141f6d13c9df17ee385
zoople <zoople@Muhammeds-MBP.lan>
2025-07-29 17:47:03 +0530

    AdarshView add two btn and one image

--- gitcollab/AdarshView.swift
+++ gitcollab/AdarshView.swift
@@ -9,9 +9,25 @@ import SwiftUI
 struct AdarshView: View {
     var body: some View {
         VStack {
-
+            HStack{
+           Image(systemName: "arrowshape.backward.circle")
+                Spacer()
+                Text("Now Playing")
+                Spacer()
+                Image("share")
+            }
+            VStack{
+                Image("imagebg")
+                    .resizable()
+                    .frame(width: 200,height: 200)
+                    .cornerRadius(20)
+            }
+            
         }
         .navigationTitle("Adarsh")
         .navigationBarTitleDisplayMode(.inline)
     }
+}
+#Preview {
+    AdarshView()
 }
